b33c3d08b895a1afc1dfc7e6f3ddc03f38f9e3aa
Mosquitod <Mosquitod3@yandex.ru>
2021-05-26 11:07:17 +0300

    Парсинг HTML страницы refactoring

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -10,14 +10,13 @@ public class SqlRuParse {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
         Elements data = doc.select(".altCol");
-        int index = 0;
+        int index = 1;
         for (Element td : row) {
             Element href = td.child(0);
             Element date = data.get(index);
-            System.out.printf("Описание: %s\nСсылка: %s\n", href.text(), href.attr("href"));
-            System.out.printf("Дата добавления: %s\n", date.text());
+            System.out.printf("Описание: %s%nСсылка: %s%nДата добавления: %s%n", href.text(), href.attr("href"), date.text());
             System.out.println("----------------------------------------------------------------------");
-            index++;
+            index += 2;
         }
     }
 }
