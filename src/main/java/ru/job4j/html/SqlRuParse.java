56231bb8a3595247f66e893510d7544919d142cd
Mosquitod <Mosquitod3@yandex.ru>
2021-06-06 20:07:08 +0300

    Загрузка деталей поста.

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -7,28 +7,34 @@ import org.jsoup.select.Elements;
 import ru.job4j.utils.DateTimeParser;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
+import java.io.IOException;
 import java.time.format.DateTimeFormatter;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        DateTimeParser parserDate = new SqlRuDateTimeParser();
         String urlPages = "https://www.sql.ru/forum/job-offers/";
         for (int i = 1; i < 6; i++) {
             Document doc = Jsoup.connect(urlPages + i).get();
             Elements row = doc.select(".postslisttopic");
-            Elements data = doc.select(".altCol");
-            int index = 1;
             for (Element td : row) {
-                Element href = td.child(0);
-                Element date = data.get(index);
-                System.out.printf("Описание: %s%nСсылка: %s%nДата добавления: %s%n",
-                        href.text(),
-                        href.attr("href"),
-                        parserDate.parse(date.text()).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"))
-                );
-                System.out.println("----------------------------------------------------------------------");
-                index += 2;
+                Post post = detail(td.child(0).attr("href"));
+                System.out.printf("Name: %s%nLink: %s%nText: %s%nDate: %s%n",
+                        post.getName(),
+                        post.getLink(),
+                        post.getText(),
+                        post.getDate().format(DateTimeFormatter.ofPattern("dd MMM yy, HH:mm")));
             }
         }
+    }
+
+    private static Post detail(String url) throws IOException {
+        DateTimeParser parserDate = new SqlRuDateTimeParser();
+        Document doc = Jsoup.connect(url).get();
+        String link = url;
+        String name = doc.selectFirst(".messageHeader").text();
+        String text = doc.select(".msgBody").get(1).text();
+        String formatDate = doc.select(".msgFooter").text();
+        String date = formatDate.substring(0, formatDate.indexOf("[")).trim();
+        return new Post(name, text, link, parserDate.parse(date));
     }
 }
