dfc3cb2bb812cbf9ce5a004f29ff22086a8d7a3f
Mosquitod <Mosquitod3@yandex.ru>
2021-06-07 21:05:19 +0300

    SqlRuParse

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,32 +4,56 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.utils.DateTimeParser;
 import ru.job4j.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
     public static void main(String[] args) throws Exception {
+        SqlRuParse sql = new SqlRuParse();
         String urlPages = "https://www.sql.ru/forum/job-offers/";
+        List<Post> posts = null;
         for (int i = 1; i < 6; i++) {
-            Document doc = Jsoup.connect(urlPages + i).get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element td : row) {
-                Post post = detail(td.child(0).attr("href"));
-                System.out.printf("Name: %s%nLink: %s%nText: %s%nDate: %s%n",
-                        post.getName(),
-                        post.getLink(),
-                        post.getText(),
-                        post.getDate().format(DateTimeFormatter.ofPattern("dd MMM yy, HH:mm")));
-            }
+            posts = sql.list(urlPages + i);
+        }
+        for (Post post : posts) {
+            System.out.printf("Name: %s%nLink: %s%nText: %s%nDate: %s%n",
+                    post.getName(),
+                    post.getLink(),
+                    post.getText(),
+                    post.getDate().format(DateTimeFormatter.ofPattern("dd MMM yy, HH:mm")));
         }
     }
 
-    private static Post detail(String url) throws IOException {
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
+        try {
+            Document doc = Jsoup.connect(link).get();
+            Elements row = doc.select(".postslisttopic");
+            for (Element td : row) {
+                list.add(detail(td.child(0).attr("href")));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
+
+    @Override
+    public Post detail(String url) {
         DateTimeParser parserDate = new SqlRuDateTimeParser();
-        Document doc = Jsoup.connect(url).get();
+        Document doc = null;
+        try {
+            doc = Jsoup.connect(url).get();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         String link = url;
         String name = doc.selectFirst(".messageHeader").text();
         String text = doc.select(".msgBody").get(1).text();
